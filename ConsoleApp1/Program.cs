f77b9f34a04a16de2900f8b41429ccf8fed9f422
Paulseit <paulseitner@yahoo.com>
2025-03-30 20:19:59 -0400

    added console app1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ConsoleApp1
+{
+    public class MathHelper
+    {
+        public int Add(int a, int b) => a + b;
+        public int Multiply(int a, int b) => a * b;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var math = new MathHelper();
+            Console.WriteLine("2 + 3 = " + math.Add(2, 3));
+            Console.WriteLine("4 * 5 = " + math.Multiply(4, 5));
+        }
+    }
+}
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b898cd15949e08291f15a095e77e4fd461360f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad8791dbc525fdd1d1c040ed0258d900d1c16996")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
